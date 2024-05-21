46a1b56ca2cad5806ea4efd4a0ac2ffef493efff
Stakan13 <mkbs1910@gmail.com>
2024-05-22 01:16:45 +0300

    All commit

--- MusculeHill/Coach.h
+++ MusculeHill/Coach.h
@@ -5,7 +5,7 @@ class Coach : public ClubMember {
 protected:
 	int experience;
 public:
-	Coach(std::string firstName, std::string middlename, int userAge, std::string memberEmail, std::string pASSword, char sexxx, bool abonementType) : ClubMember(
+	Coach(std::string firstName, std::string middlename, int userAge, std::string memberEmail, std::string pASSword, char sexxx, bool abonementType, int experience) : ClubMember(
 		firstName, middlename, userAge, memberEmail, pASSword, sexxx, abonementType), experience(experience) {
 	}
 
